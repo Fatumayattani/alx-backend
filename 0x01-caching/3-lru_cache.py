ade6e117b7a8bb1687d6483424a055e0aa601810
Fatumayattani <fyattani@gmail.com>
2023-07-26 08:28:31 -0700

    save changes

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
-""" LIFOCache module
+""" LRUCache module
 """
 
 from base_caching import BaseCaching
-from collections import OrderedDict
 
-class LIFOCache(BaseCaching):
-    """ LIFOCache class that inherits from BaseCaching """
+class LRUCache(BaseCaching):
+    """ LRUCache class that inherits from BaseCaching """
     
     def __init__(self):
-        """ Initialize LIFOCache """
+        """ Initialize LRUCache """
         super().__init__()
-        self.order_of_insertion = []
+        self.order_of_access = []
 
     def put(self, key, item):
         """ Add an item in the cache """
         if key is not None and item is not None:
             if len(self.cache_data) >= self.MAX_ITEMS:
-                last_item = self.order_of_insertion.pop()
-                self.cache_data.pop(last_item)
-                print("DISCARD:", last_item)
+                lru_item = self.order_of_access.pop(0)
+                self.cache_data.pop(lru_item)
+                print("DISCARD:", lru_item)
             self.cache_data[key] = item
-            self.order_of_insertion.append(key)
+            self.order_of_access.append(key)
 
     def get(self, key):
         """ Get an item by key """
         if key is not None and key in self.cache_data:
+            self.order_of_access.remove(key)
+            self.order_of_access.append(key)
             return self.cache_data[key]
         return None
